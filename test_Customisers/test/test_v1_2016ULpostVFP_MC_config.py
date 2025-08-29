8e36059e41e932606e3974ea48aeab0b8e634054
xondikoi03 <raj.handique519@deccansociety.org>
2025-08-29 14:49:28 +0530

    Workflow Fix 1.6

--- test_Customisers/test/test_v1_2016ULpostVFP_MC_config.py
+++ test_Customisers/test/test_v1_2016ULpostVFP_MC_config.py
@@ -3,6 +3,7 @@
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
 # with command line options: --python_filename test_v1_2016ULpostVFP_MC_config.py --fileout ../outputfiles/nanoMCTest.root -s NANO --mc --conditions 106X_mcRun2_asymptotic_v17 --era Run2_2016,run2_nanoAOD_106Xv2 --eventcontent NANOAODSIM --datatier NANOAODSIM --customise_commands from nanoAODCustomisation.    test_Customisers.customiser_cff import nanoAODCustomise_DeepInfoAK8;     import FWCore.Utilities.FileUtils as FileUtils;     files = FileUtils.loadListFromFile('../datasets/test_1.txt');     process.source = cms.Source('PoolSource',     fileNames = cms.untracked.vstring(*files),     secondaryFileNames = cms.untracked.vstring());     process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)));     process.MessageLogger.cerr.FwkReport.reportEvery = 1000;     process.load('FWCore.MessageLogger.MessageLogger_cfi');     process.MessageLogger.cerr.threshold = 'INFO';     process.MessageLogger.cerr.INFO = cms.untracked.PSet(limit = cms.untracked.int32(-1)); --nThreads 16 -n -1 --no_exec
+
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run2_2016_cff import Run2_2016
@@ -23,11 +24,11 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
 )
 
 import FWCore.Utilities.FileUtils as FileUtils
-files = FileUtils.loadListFromFile('../datasets/test_1.txt')     
+files = FileUtils.loadListFromFile('../datasets/test_2.txt')     
 process.source = cms.Source('PoolSource',
                             fileNames = cms.untracked.vstring(*files),
                             secondaryFileNames = cms.untracked.vstring())
