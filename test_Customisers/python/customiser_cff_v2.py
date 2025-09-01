04c908f6ec1e84a2425402d36233156e66041f49
xondikoi03 <raj.handique519@deccansociety.org>
2025-09-01 23:32:41 +0530

    TauID & BoostedTauID Customisers fix 1.0 & Workflow fixes 6.8

--- test_Customisers/python/customiser_cff_v2.py
+++ test_Customisers/python/customiser_cff_v2.py
@@ -2,29 +2,39 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.NanoAOD.nano_cff import *
 
 def default_TauIdsCustomisation(process):
+    print("###########################################")
+    print("Default TauID Customisations")
+    print("###########################################")
+    from PhysicsTools.NanoAOD.nano_cff import nanoAOD_activateVID
+    process = nanoAOD_activateVID(process)    
     # By Default this adds only "deepTauId2017v2p1"
     from PhysicsTools.NanoAOD.nano_cff import nanoAOD_addTauIds    
-    nanoAOD_addTauIds(process)
+    process = nanoAOD_addTauIds(process)
 
     # By Default this add only "2017v2", "dR0p32017v2", "newDM2017v2", "againstEle2018"
 
     from PhysicsTools.NanoAOD.nano_cff import nanoAOD_addBoostedTauIds
-    nanoAOD_addBoostedTauIds(process)
+    process = nanoAOD_addBoostedTauIds(process)
 
     # Let's see how this changes or adds anything new to the tau table
     return process
 
 def Customise_TauIdsCustomisation(process):
+    print("###########################################")
+    print("Running TauID Customisations")
+    print("###########################################")
+    from PhysicsTools.NanoAOD.nano_cff import nanoAOD_activateVID
+    process = nanoAOD_activateVID(process)
     import RecoTauTag.RecoTau.tools.runTauIdMVA as tauIdConfig
     def nanoAOD_customiseAddTauIds(process):
         updatedTauName = "slimmedTausUpdatedCustom"
         tauIdEmbedder = tauIdConfig.TauIDEmbedder(
             process, cms, debug=False, updatedTauName=updatedTauName,
-            toKeep = ["deepTau2017v1", "deepTau2017v2", "DPFTau_2016_v0", "DPFTau_2016_v1"])
+            toKeep = ["deepTau2017v1", "deepTau2017v2"])
         tauIdEmbedder.runTauID()
         process.patTauMVAIDsSeq.index(getattr(process, updatedTauName)), process.rerunMvaIsolationSequence
         return process
-    nanoAOD_customiseAddTauIds(process)
+    process = nanoAOD_customiseAddTauIds(process)
 
     def nanoAOD_customiseAddBoostedIds(process):
         updatedBoostedTauName = "slimmedTausBoostedNewIDCustom"
@@ -41,7 +51,7 @@ def Customise_TauIdsCustomisation(process):
                                         getattr(process, updatedBoostedTauName))
 
         return process    
-    nanoAOD_customiseAddBoostedIds(process)
+    process = nanoAOD_customiseAddBoostedIds(process)
 
     return process
 
--- test_Customisers/test/test_v1_2016ULpostVFP_MC_config_0.py
+++ test_Customisers/test/test_v1_2016ULpostVFP_MC_config_0.py
@@ -81,10 +81,10 @@ process.options.numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(1)
 # customisation of the process.
 
 # Automatic addition of the customisation function from PhysicsTools.NanoAOD.nano_cff
-from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeMC 
+#from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeMC 
 
 #call to customisation function nanoAOD_customizeMC imported from PhysicsTools.NanoAOD.nano_cff
-process = nanoAOD_customizeMC(process)
+#process = nanoAOD_customizeMC(process)
 
 # End of customisation functions
 
